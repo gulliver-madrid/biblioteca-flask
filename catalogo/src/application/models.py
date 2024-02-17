e89f70c28a89a9f2436bb356399b40add46ceaa4
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-02-17 14:47:06 +0100

    format models.py

--- catalogo/src/application/models.py
+++ catalogo/src/application/models.py
@@ -3,21 +3,25 @@ from . import db
 
 # aqui se definen los modelos
 
-autores_libros = db.Table('autores_libros',
-    db.Column('autor_id', db.Integer, db.ForeignKey('autores.id'), primary_key=True),
-    db.Column('libro_id', db.Integer, db.ForeignKey('libros.id'), primary_key=True)
+autores_libros = db.Table(
+    "autores_libros",
+    db.Column("autor_id", db.Integer, db.ForeignKey("autores.id"), primary_key=True),
+    db.Column("libro_id", db.Integer, db.ForeignKey("libros.id"), primary_key=True),
 )
 
+
 class Autor(db.Model):
-    __tablename__ = 'autores'
+    __tablename__ = "autores"
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(255), nullable=False)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
     # Relacion inversa (opcional)
-    libros = db.relationship('Libro', secondary=autores_libros, backref=db.backref('autores', lazy='dynamic'))
+    libros = db.relationship(
+        "Libro", secondary=autores_libros, backref=db.backref("autores", lazy="dynamic")
+    )
 
     def __repr__(self) -> str:
-        return f"<Autor(id={self.id}, nombre=\"{self.nombre}\")>"
+        return f'<Autor(id={self.id}, nombre="{self.nombre}")>'
 
     def to_json(self):
         return {
@@ -28,14 +32,14 @@ class Autor(db.Model):
 
 
 class Libro(db.Model):
-    __tablename__ = 'libros'
+    __tablename__ = "libros"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), unique=True, nullable=False)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
     # La relacion con autores se maneja a traves de la tabla asociativa
 
     def __repr__(self) -> str:
-        return f"<Libro(id={self.id}, title=\"{self.title}\")>"
+        return f'<Libro(id={self.id}, title="{self.title}")>'
 
     def to_json(self):
         return {
