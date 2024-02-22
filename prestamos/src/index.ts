97f88840de9671fc250de67f4607e47289ae07e1
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-02-22 11:25:35 +0100

    prestamos: use cors

--- prestamos/src/index.ts
+++ prestamos/src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express'
+import cors from 'cors'
 
 import prestamosRouter from './routes/loans'
 
 const PORT = 3000
+const FRONTEND_PORT = 5173
 
 const app = express()
 
 app.use(express.json()) // middleware que transforma la req. body a un json
+
+app.use(cors({ origin: `http://localhost:${FRONTEND_PORT}` }))
 
 app.use('/api/loans', prestamosRouter)
 
