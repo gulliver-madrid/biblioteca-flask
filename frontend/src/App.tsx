4a3c4846af276e264a2248caedbf3d9ca4a50320
gulliver-madrid <49131885+gulliver-madrid@users.noreply.github.com>
2024-02-22 11:35:53 +0100

    fetch data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,36 @@
+import { useEffect } from 'react'
 import './App.css'
 
+async function fetchData() {
+  try {
+    // Recuperar la lista de prestamos
+    const responsePrestamos = await fetch('http://localhost:3000/api/loans');
+    if (!responsePrestamos.ok) {
+    console.log("error prestamos");
+      throw new Error('Error al recuperar los préstamos');
+    }
+    const prestamos = await responsePrestamos.json();
+    console.log('Préstamos:', prestamos);
+
+    // Recuperar la lista de libros
+    const responseLibros = await fetch('http://localhost:7001/api/libros');
+    if (!responseLibros.ok) {
+    console.log("error libros");
+      throw new Error('Error al recuperar los libros');
+    }
+    const libros = await responseLibros.json();
+    console.log('Libros:', libros);
+  } catch (error) {
+    console.error('Hubo un error en la solicitud:', error);
+  }
+}
+
+
+
 function App() {
+  useEffect(()=>{
+    fetchData()
+  })
   return (
     <>
       <h1>Biblioteca</h1>
